470161eb5c6785556bf949e19d088167c620c0ed
Paw Oktober <pawhovsgaard@gmail.com>
2021-08-10 15:59:23 +0200

    starting analytics for data collection

---
+++ CardGamePrototype/Assets/Scripts/Analytics/DataCollection.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameLogic;
+using Event = GameLogic.Event;
+using UnityEngine.Analytics;
+using System;
+using System.Linq;
+
+public class DataCollection : MonoBehaviour
+{
+#if ENABLE_CLOUD_SERVICES_ANALYTICS
+
+    void Awake()
+    {
+        Event.OnHeroSelect.AddListener(SendHeroSelect);
+        Event.OnGameBegin.AddListener(SendGameBegin);
+        Event.OnGameWin.AddListener(SendGameOver);
+        Event.OnBattleFinished.AddListener(SendBattleData);
+    }
+
+    private void SendBattleData(Deck arg0, Deck arg1)
+    {
+    }
+
+    private void SendGameWin()
+    {
+
+    }
+
+    private void SendGameOver()
+    {
+    }
+
+    private void SendGameBegin()
+    {
+    }
+
+    private void SendHeroSelect(Hero hero)
+    {
+        Analytics.CustomEvent("HeroSelected", new Dictionary<string, object>
+        {
+            { "Selected", hero.HeroObject.name}
+        });
+    }
+
+    private void SendCustomEvent(string eventName, params KeyValuePair<string, object>[] eventData )
+    {
+        Analytics.CustomEvent(eventName, eventData.ToDictionary(pair => pair.Key,pair => pair.Value));
+    }
+
+
+
+#endif
+}
