d9dc34e1565e367807b82313fa595ed68a382c9e
Paw Oktober <pawhovsgaard@gmail.com>
2021-08-17 21:59:56 +0200

    Better Colors

---
+++ CardGamePrototype/Assets/ColorSelector.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSelector : MonoBehaviour
+{
+    public Color Assassin, Cleric, Renegade, Summoner, Tactician, Wizard, Beastfolk, Commonfolk, Undead, Wildfolk;
+}
